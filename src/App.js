9e0c53151843352ccdfda8abf3de77930ea9f22d
Jenny Oum <jennyoum@Users-MacBook-Air.local>
2019-09-05 23:47:31 -0400

    fixed image layout, clickable images, adds to score. need to fix shuffle cards, and status messages

--- src/App.js
+++ src/App.js
@@ -14,7 +14,19 @@ class App extends Component {
   }
 
 clickedCards = id => {
+  if( this.state.clicked.includes(id)){
+    this.setState({clicked: [], score: +1, message: "Great job! Keep going!!!"});
+  
+  }
   this.setState({score: this.state.score + 1 });
+  this.shuffleCards();
+};
+
+shuffleCards = id => {
+  for (let i = 0; i > 12; i++){
+    let shuffleOrder = Math.floor(Math.random() * 12)
+    this.setState({shuffleOrder})
+  }
 };
 
 render() {
@@ -22,16 +34,20 @@ render() {
     <Wrapper>
     <Nav>
       Score:  {this.state.score}
+      <br></br>
       Top Score:  {this.state.topScore}
+      <br></br>
       Status:  {this.state.message}
     </Nav>
-    <div>
-      {this.state.images.map(card => ( <div><img src={card.image} alt={card.id}></img></div>))}
-    </div>
+    
+      {this.state.images.map(card => ( 
+      <ScoutsCard 
+      image={card.image} alt={card.id} clickedCards={this.clickedCards}/>))}
+    
     </Wrapper>
   );
 };
 
 };
 
-export default App;
+export default App; 
--- src/components/ScoutsCard/index.js
+++ src/components/ScoutsCard/index.js
@@ -1,20 +1,11 @@
 import React from "react";
 import "./style.css";
 
-const ScoutsCard = props => {
-  return (
-    <div>
-    <div className="row">
-      <div className="col-md-3">
+const ScoutsCard = props => (
         <div className="card">
-          <div className="img-container">
-            <img className="card-img" alt={props.id} src={props.image} />
-          </div>
+            <img alt={props.id} src={props.image} id={props.id} onClick={()=> props.clickedCards(props.id)}/>
         </div>
-      </div>
-    </div>
-    </div>
   );
-}
 
-export default ScoutsCard;
+
+export default ScoutsCard; 
